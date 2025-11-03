352d4735ed257e71d6bc0cceaa55f6593369d06a
rob <roby.pakalns@gmail.com>
2025-11-03 17:26:47 +0200

    tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 #![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 use webview2_com::{Microsoft::Web::WebView2::Win32::*, *};
 use windows::{
-    Win32::{
-        Foundation::*, Graphics::Gdi::*, System::LibraryLoader::*, UI::WindowsAndMessaging::*,
-    },
+    Win32::{Foundation::*, System::LibraryLoader::*, UI::WindowsAndMessaging::*},
     core::*,
 };
-
-pub fn create_utf_string(str: &str) -> Vec<u16> {
-    let mut s: Vec<u16> = str.encode_utf16().collect();
-    s.push(0);
-    s
-}
 
 pub fn create_window() -> HWND {
     unsafe {
         let hinstance: HINSTANCE = GetModuleHandleW(None).unwrap().into();
-        let icon = match LoadIconW(Some(hinstance), w!("icon")) {
-            Ok(icon) => icon,
-            Err(_) => LoadIconW(None, IDI_APPLICATION).unwrap(),
-        };
 
         let class_name = w!("webview2_client");
         let wc = WNDCLASSW {
@@ -28,9 +16,9 @@ pub fn create_window() -> HWND {
             cbClsExtra: 0,
             cbWndExtra: 0,
             hInstance: hinstance,
-            hIcon: icon,
+            hIcon: Default::default(),
             hCursor: Default::default(),
-            hbrBackground: CreateSolidBrush(COLORREF(0x00000000)),
+            hbrBackground: Default::default(),
             lpszMenuName: PCWSTR::null(),
             lpszClassName: class_name,
         };
@@ -43,7 +31,7 @@ pub fn create_window() -> HWND {
         let x = (GetSystemMetrics(SM_CXSCREEN) - width) / 2;
         let y = (GetSystemMetrics(SM_CYSCREEN) - height) / 2;
 
-        let hwnd: HWND = CreateWindowExW(
+        CreateWindowExW(
             WINDOW_EX_STYLE::default(),
             class_name,
             w!("WebView2 Client"),
@@ -57,25 +45,18 @@ pub fn create_window() -> HWND {
             Some(hinstance),
             None,
         )
-        .unwrap();
-
-        hwnd
+        .unwrap()
     }
 }
 
 pub fn create_webview2(hwnd: HWND) -> (ICoreWebView2Controller4, ICoreWebView2_22) {
     unsafe {
-        let options: CoreWebView2EnvironmentOptions = CoreWebView2EnvironmentOptions::default();
+        let options = CoreWebView2EnvironmentOptions::default();
         options.set_additional_browser_arguments("--disable-frame-rate-limit".to_string());
 
         let (tx, rx) = std::sync::mpsc::channel();
-        let mut current_exe = std::env::current_exe().unwrap();
-        current_exe.pop();
 
-        let mut exe_dir = std::env::current_exe().unwrap();
-        exe_dir.pop();
-
-        let result = CreateCoreWebView2EnvironmentCompletedHandler::wait_for_async_operation(
+        CreateCoreWebView2EnvironmentCompletedHandler::wait_for_async_operation(
             Box::new(move |environment_created_handler| {
                 CreateCoreWebView2EnvironmentWithOptions(
                     PCWSTR::null(),
@@ -109,11 +90,8 @@ pub fn create_webview2(hwnd: HWND) -> (ICoreWebView2Controller4, ICoreWebView2_2
                 .unwrap();
                 Ok(())
             }),
-        );
-
-        if result.is_err() {
-            panic!("Failed to create WebView2 env: {:?}", result)
-        };
+        )
+        .expect("Failed to create WebView2 env");
 
         let controller = rx
             .recv()
@@ -157,9 +135,8 @@ unsafe extern "system" fn wnd_proc_main(
                 let ptr = GetWindowLongPtrW(hwnd, GWLP_USERDATA) as *mut ICoreWebView2Controller4;
                 if !ptr.is_null() {
                     let mut rect = RECT::default();
-                    let controller = &*ptr;
                     GetClientRect(hwnd, &mut rect).ok();
-                    controller.SetBounds(rect).ok();
+                    (*ptr).SetBounds(rect).ok();
                 }
             }
             LRESULT(0)
@@ -179,34 +156,24 @@ fn main() {
 
         SetWindowLongPtrW(hwnd, GWLP_USERDATA, &controller as *const _ as isize);
 
+        let script = HSTRING::from(include_str!("script.js"));
         webview
-            .AddScriptToExecuteOnDocumentCreated(
-                PCWSTR(create_utf_string(include_str!("script.js")).as_ptr()),
-                None,
-            )
+            .AddScriptToExecuteOnDocumentCreated(PCWSTR(script.as_ptr()), None)
             .ok();
 
         webview.Navigate(w!("https://kirka.io")).ok();
 
         webview
             .add_WebMessageReceived(
-                &WebMessageReceivedEventHandler::create(Box::new(
-                    move |_, args: Option<ICoreWebView2WebMessageReceivedEventArgs>| {
-                        let args = args.unwrap();
-                        let mut message_vec = create_utf_string("");
-                        let message = message_vec.as_mut_ptr() as *mut PWSTR;
-                        args.TryGetWebMessageAsString(message).ok();
-                        let message_string = message.as_ref().unwrap().to_string().unwrap();
-
-                        let parts: Vec<&str> =
-                            message_string.split(", ").map(|s| s.trim()).collect();
-                        if parts.first() == Some(&"close") {
-                            PostQuitMessage(0);
-                        }
-                        Ok(())
-                    },
-                )),
-                0 as *mut i64,
+                &WebMessageReceivedEventHandler::create(Box::new(move |_, args| {
+                    let mut msg = PWSTR::null();
+                    args.unwrap().TryGetWebMessageAsString(&mut msg).ok();
+                    if msg.to_string()?.trim_matches('"') == "close" {
+                        PostQuitMessage(0);
+                    }
+                    Ok(())
+                })),
+                std::ptr::null_mut(),
             )
             .ok();
 
